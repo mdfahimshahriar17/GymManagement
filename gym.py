b32669bd7260376cd3e083caad5ec3db640b5773
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-07 00:09:01 +0600

    Added Gym class and add member method

--- gym.py
+++ gym.py
@@ -10,7 +10,7 @@ class Person:
     def __str__(self):
         return f"Name : {self.name}\nAge : {self.age}\nHeight : {self.height}\nWeight: {self.weight}\nBMI : {self.BMI}"
 
-class Calculate_BMI:
+class BMI_Calculate:
 
     def calculate_bmi(self, height, weight):
         
@@ -30,6 +30,37 @@ class Calculate_BMI:
             bmi_range = "Obese"
 
         return (BMI, bmi_range)
+
+
+
+class GYM:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.member_informations = []
+
+
+
+
+    def add_member(self, name, age, phone, height, weight):
+        bmi = BMI_Calculate().calculate_bmi(height=height, weight=weight)
+
+        person = Person(name=name, age=age, phone=phone, height=height, weight=weight, BMI=bmi)
+        
+        self.member_informations.append(person)
+
+
+gym = GYM("Gymnasium", "Hussaini Dalan")
+
+gym.add_member("Fahim", 26, "012345821", 1.68, 85.5)
+gym.add_member("Nahar", 26, "012615821", 1.23, 56)
+
+print([str(member) for member in gym.member_informations])
+
+
+
+
+
 
 
 
